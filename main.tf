1b2e0553a45e8cfacdacf781670b9361d1f23a13
auto-store <37550823+auto-store@users.noreply.github.com>
2020-03-17 18:01:58 +0000

    changed vars

--- main.tf
+++ main.tf
@@ -12,11 +12,11 @@ resource "google_compute_instance" "consul-server" {
 
   provisioner "remote-exec" {
     inline = [
-       "sudo useradd --system --home /etc/consul.d --shell /bin/false consul"
-       "sudo mkdir --parents /opt/consul"
-       "sudo chown --recursive consul:consul /opt/consul" 
-       "sudo git clone https://github.com/auto-store/GCE-instance /etc/ansible/"
-       "sudo ansible-playbook /etc/ansible/GCE-instance/consul.yml -e "encrypt=${var.encrypt}" "project=${var.project}""
+       "sudo useradd --system --home /etc/consul.d --shell /bin/false consul",
+       "sudo mkdir --parents /opt/consul",
+       "sudo chown --recursive consul:consul /opt/consul", 
+       "sudo git clone https://github.com/auto-store/GCE-instance /etc/ansible/",
+       "sudo ansible-playbook /etc/ansible/GCE-instance/consul.yml -e encrypt=${var.encrypt} project=${var.project}"
     ]
      connection {
       type        = "ssh"
