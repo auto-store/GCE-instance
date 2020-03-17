7e44e8ace386302edeb50d1b7561cb9a7b2004dc
auto-store <37550823+auto-store@users.noreply.github.com>
2020-03-17 21:48:27 +0000

    Changed connection

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@ resource "google_compute_instance" "consul-server" {
        "sudo git clone https://github.com/auto-store/GCE-instance /etc/ansible/GCE-instance",
        "sudo ansible-playbook /etc/ansible/GCE-instance/files/consul.yml"
     ]
-     connection files{
+     connection {
       type        = "ssh"
       user        = var.ssh_user  
       private_key = var.private_key
-       host        = self.network_interface[0].access_config[0].nat_ip
+      host        = self.network_interface[0].access_config[0].nat_ip
     }
 
   }
