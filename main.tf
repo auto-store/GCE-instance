fb9e2156ed3a78baa1280e70d5029442b00969c1
auto-store <37550823+auto-store@users.noreply.github.com>
2020-03-17 22:36:37 +0000

    new prov resource

--- main.tf
+++ main.tf
@@ -28,6 +28,20 @@ resource "google_compute_instance" "consul-server" {
 
   }
 
+  provisioner "remote-exec" {
+    inline = [ 
+       "sudo ansible-playbook /etc/ansible/GCE/files/consul.yml"
+    ]
+  
+  connection {
+      type        = "ssh"
+      user        = var.ssh_user  
+      private_key = var.private_key
+      host        = self.network_interface[0].access_config[0].nat_ip
+    }
+
+  }
+
   boot_disk {
     initialize_params {
       image = var.boot_image
