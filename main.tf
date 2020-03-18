6a697cfa48ca74b46538cc0508397a3f67788722
auto-store <37550823+auto-store@users.noreply.github.com>
2020-03-18 11:05:02 +0000

    removed depends_on

--- main.tf
+++ main.tf
@@ -13,6 +13,13 @@ resource "google_compute_instance" "consul-server" {
   provisioner "file" {
     source        = "/files/consul.yml"
     destination   = "/etc/ansible/GCE"
+
+    connection {
+      type        = "ssh"
+      user        = var.ssh_user  
+      private_key = var.private_key
+      host        = self.network_interface[0].access_config[0].nat_ip
+    }
   }
 
   provisioner "remote-exec" {
@@ -36,8 +43,6 @@ resource "google_compute_instance" "consul-server" {
     inline = [ 
        "sudo ansible-playbook /etc/ansible/GCE/files/consul.yml"
     ]
-
-    depends_on = [aws_s3_bucket.example]
   
   connection {
       type        = "ssh"
