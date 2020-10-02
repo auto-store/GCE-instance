25608027129b940f42d54e1bb5faddad005e82a1
auto-store <37550823+auto-store@users.noreply.github.com>
2020-10-02 15:45:59 +0100

    new port

--- nomad-cluster2.tf
+++ nomad-cluster2.tf
@@ -20,18 +20,18 @@ resource "google_compute_instance" "nomad-server-2" {
 
   service_account {
     scopes = ["cloud-platform"]
-  } 
+  }
 
-  tags = ["dr"]  
+  tags = ["dr"]
 
   provisioner "remote-exec" {
     inline = [
        "sudo git clone https://github.com/auto-store/GCE-instance /home/GCE-instance"
     ]
-  
+
   connection {
       type        = "ssh"
-      user        = var.ssh_user  
+      user        = var.ssh_user
       private_key = var.private_key
       host        = self.network_interface[0].access_config[0].nat_ip
     }
